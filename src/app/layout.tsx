81453d37cdbfea2fc8db9db431f1fa4ad1084456
R5dan <R5dan@outlook.com>
2024-11-24 14:48:15 +0000

    Add auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,13 @@
 import "~/styles/globals.css";
-
 import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton
+} from '@clerk/nextjs'
 
 export const metadata: Metadata = {
   title: "Create T3 App",
@@ -13,8 +19,22 @@ export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" className={`${GeistSans.variable}`}>
-      <body>{children}</body>
-    </html>
-  );
+    <ClerkProvider>
+        <html lang="en" className={`${GeistSans.variable}`}>
+          <body>
+            <nav>
+              <SignedOut>
+                <SignInButton />
+              </SignedOut>
+              <SignedIn>
+                <UserButton />
+              </SignedIn>
+            </nav>
+            <div>
+              {children}
+            </div>
+          </body>
+        </html>
+    </ClerkProvider>  
+);
 }
